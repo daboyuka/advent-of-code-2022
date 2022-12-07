1f79db3cd1189b395c4fac8af4d0eb3792a4406b
Drew Boyuka <drew@pendo.io>
2022-12-07 00:12:50 -0500

    Day 7

---
+++ p07/a.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+from helpers import *
+from itertools import *
+from collections import *
+from functools import *
+import re
+
+t = dict()
+
+stack = [t]
+
+for l in lines():
+    words = l.split(" ")
+
+    if words[0] == "$":
+        if words[1] == "ls":
+            pass
+        elif words[1] == "cd":
+            if words[2] == "/":
+                stack = [t]
+            elif words[2] == "..":
+                stack.pop()
+            else:
+                dirname = words[2]
+                dir = dict()
+                stack[-1].pop(dirname, None) # clear dir
+                stack[-1][dirname] = dir
+                stack.append(dir)
+    elif words[0] == "dir":
+        pass
+    else:
+        size, filename = int(words[0]), words[1]
+        stack[-1][filename] = size
+
+
+grandtotal = 0
+
+def getsize(dir):
+    global grandtotal
+    total = 0
+    for k, v in dir.items():
+        if isinstance(v, dict):
+            total += getsize(v)
+        else:
+            total += v
+    if total <= 100000:
+        grandtotal += total
+    return total
+
+print(getsize(t))
+print(grandtotal)
+
